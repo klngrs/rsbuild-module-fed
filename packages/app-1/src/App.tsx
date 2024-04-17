65a28ad8ba267c567ff47bf7cbe84159992058d9
Tom Kleingers <tom@kleingers.us>
2024-04-16 22:49:17 -0400

    Added workspaces. Two apps. Producer + consumer working

---
+++ packages/app-1/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Button from './button';
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>App 1 - Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+        <Button></Button>
+    </div>
+  );
+};
+
+export default App;
