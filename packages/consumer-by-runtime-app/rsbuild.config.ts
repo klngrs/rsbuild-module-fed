e9e3c0f683683f4a9e9dfd3a77e5e4c77824efb9
Tom Kleingers <tom@kleingers.us>
2024-04-17 21:58:35 -0400

    Add runtime consumer app bootstrap

--- packages/consumer-by-runtime-app/rsbuild.config.ts
+++ packages/consumer-by-runtime-app/rsbuild.config.ts
@@ -4,22 +4,19 @@ import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
 
 export default defineConfig({
   server: {
-    port: 3000,
-  },
-  dev: {
-    // It is necessary to configure assetPrefix, and in the production environment, you need to configure output.assetPrefix
-    assetPrefix: 'http://localhost:3000',
+    port: 1000,
   },
   tools: {
     rspack: (config, { appendPlugins }) => {
       appendPlugins([
-        new ModuleFederationPlugin({
-          name: 'federation_provider',
-          exposes: {
-            './button': './src/button.tsx',
+        /*new ModuleFederationPlugin({
+          name: 'federation_consumer',
+          remotes: {
+            federation_provider:
+                'federation_provider@http://localhost:3000/mf-manifest.json',
           },
           shared: ['react', 'react-dom'],
-        }),
+        }),*/
       ]);
     },
   },
---
+++ packages/consumer-by-runtime-app/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+// @ts-ignore
+import ProviderButton from 'federation_provider/button';
+
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>App 2 - Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+        <ProviderButton />
+    </div>
+  );
+};
+
+export default App;
